90b55434824c828cb720567ca4ec33616b9e2069
NATA ARIFAH TEKNIK <ilhamdwiprnmo@gmail.com>
2023-09-27 23:08:31 +0700

    first commit to my own packages

---
+++ example/belajar_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:belajar_dart/belajar_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ example/hillo.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart/hello.dart';
+
+void main() {
+  print(sayHello("Ilham"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
